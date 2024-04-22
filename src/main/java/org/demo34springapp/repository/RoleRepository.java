6bfae1b64f7e6f50174af6ebd717c831d26a17f1
StanislavPB <perminovsb@gmail.com>
2024-04-22 14:58:12 +0100

    add: repository Role

---
+++ src/main/java/org/demo34springapp/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package org.demo34springapp.repository;
+
+import org.demo34springapp.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String roleName);
+
+}
